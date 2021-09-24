7b1add8d90fc30e5fa89389cd7911072cd51ba56
Silly-Potato <72313051+Silly-Potato@users.noreply.github.com>
2021-09-24 10:41:34 +0200

    Write book to file no longer erases previous data. updateFromJson now checks for id

--- book.py
+++ book.py
@@ -21,16 +21,26 @@ class Book:
         "year": self.year}]
 
     def writeToFile(self, path):
-        with open(path, 'w') as file:
+        with open(path, 'a') as file:
             file.write(json.dumps(self.toArray(), sort_keys=True, indent=4))
 
     def updateFromJson(self, data):
         dct = json.loads(data)
-        print(dct)
-        dct = dct[0]
-        self.id = dct['id']
-        self.name = dct["name"]
-        self.book_type = dct["type"]
-        self.genres = dct["genres"]
-        self.authors = dct["authors"]
-        self.year = dct["year"]
+        for book in dct:
+            if book['id'] == self.id:
+                self.id = dct['id']
+                self.name = dct["name"]
+                self.book_type = dct["type"]
+                self.genres = dct["genres"]
+                self.authors = dct["authors"]
+                self.year = dct["year"]
+                return
+
+        
+
+book = Book("The Way of Kings", "Novel", ["Fantasy", "Epic"], ["Brandon Sanderson"], 2010)
+with open('test.json', 'r') as file:
+    book.updateFromJson(file.read())
+
+print(book.toArray())
+#book.writeToFile("test.json")
